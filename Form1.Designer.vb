2c3e3b02689871a4e7ff404b0579511695c7d7ab
Mark Richardson <mark@dmr-ltd.co.uk>
2018-04-14 16:39:46 +0100

    Second commit, did TestDB work?

---
+++ Form1.Designer.vb
@@ -0,0 +1,428 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.txtCatalogCatalog = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.lstBackupScript = New System.Windows.Forms.ListBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtBackupConnectionString = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtBackupFileName = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtBackupDatabase = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtBackupServer = New System.Windows.Forms.TextBox()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.cmdConnect = New System.Windows.Forms.Button()
+        Me.txtConnectionConnect = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.TabPage3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.TabControl1)
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 0)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(618, 430)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(26, 403)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(107, 403)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 13
+        Me.Button2.Text = "Exit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(3, 12)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(609, 385)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.Label11)
+        Me.TabPage1.Controls.Add(Me.txtCatalogCatalog)
+        Me.TabPage1.Controls.Add(Me.Button3)
+        Me.TabPage1.Controls.Add(Me.lstBackupScript)
+        Me.TabPage1.Controls.Add(Me.Label10)
+        Me.TabPage1.Controls.Add(Me.Label2)
+        Me.TabPage1.Controls.Add(Me.txtBackupConnectionString)
+        Me.TabPage1.Controls.Add(Me.Label4)
+        Me.TabPage1.Controls.Add(Me.txtBackupFileName)
+        Me.TabPage1.Controls.Add(Me.Label3)
+        Me.TabPage1.Controls.Add(Me.txtBackupDatabase)
+        Me.TabPage1.Controls.Add(Me.Label1)
+        Me.TabPage1.Controls.Add(Me.txtBackupServer)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(601, 359)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Backup"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(16, 61)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(43, 13)
+        Me.Label11.TabIndex = 28
+        Me.Label11.Text = "Catalog"
+        '
+        'txtCatalogCatalog
+        '
+        Me.txtCatalogCatalog.Location = New System.Drawing.Point(116, 61)
+        Me.txtCatalogCatalog.Name = "txtCatalogCatalog"
+        Me.txtCatalogCatalog.Size = New System.Drawing.Size(468, 20)
+        Me.txtCatalogCatalog.TabIndex = 2
+        '
+        'Button3
+        '
+        Me.Button3.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button3.Location = New System.Drawing.Point(557, 111)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(26, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'lstBackupScript
+        '
+        Me.lstBackupScript.FormattingEnabled = True
+        Me.lstBackupScript.Location = New System.Drawing.Point(115, 140)
+        Me.lstBackupScript.Name = "lstBackupScript"
+        Me.lstBackupScript.Size = New System.Drawing.Size(468, 147)
+        Me.lstBackupScript.TabIndex = 26
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(15, 140)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(74, 13)
+        Me.Label10.TabIndex = 25
+        Me.Label10.Text = "Backup Script"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(15, 87)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(91, 13)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Connection String"
+        '
+        'txtBackupConnectionString
+        '
+        Me.txtBackupConnectionString.Location = New System.Drawing.Point(115, 87)
+        Me.txtBackupConnectionString.Name = "txtBackupConnectionString"
+        Me.txtBackupConnectionString.Size = New System.Drawing.Size(468, 20)
+        Me.txtBackupConnectionString.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(15, 113)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(89, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Backup Filename"
+        '
+        'txtBackupFileName
+        '
+        Me.txtBackupFileName.Location = New System.Drawing.Point(115, 113)
+        Me.txtBackupFileName.Name = "txtBackupFileName"
+        Me.txtBackupFileName.Size = New System.Drawing.Size(436, 20)
+        Me.txtBackupFileName.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(15, 35)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Database"
+        '
+        'txtBackupDatabase
+        '
+        Me.txtBackupDatabase.Location = New System.Drawing.Point(115, 35)
+        Me.txtBackupDatabase.Name = "txtBackupDatabase"
+        Me.txtBackupDatabase.Size = New System.Drawing.Size(468, 20)
+        Me.txtBackupDatabase.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(15, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Server"
+        '
+        'txtBackupServer
+        '
+        Me.txtBackupServer.Location = New System.Drawing.Point(115, 9)
+        Me.txtBackupServer.Name = "txtBackupServer"
+        Me.txtBackupServer.Size = New System.Drawing.Size(468, 20)
+        Me.txtBackupServer.TabIndex = 0
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.Button4)
+        Me.TabPage2.Controls.Add(Me.ListBox1)
+        Me.TabPage2.Controls.Add(Me.Label5)
+        Me.TabPage2.Controls.Add(Me.Label6)
+        Me.TabPage2.Controls.Add(Me.TextBox5)
+        Me.TabPage2.Controls.Add(Me.Label7)
+        Me.TabPage2.Controls.Add(Me.TextBox6)
+        Me.TabPage2.Controls.Add(Me.Label8)
+        Me.TabPage2.Controls.Add(Me.TextBox7)
+        Me.TabPage2.Controls.Add(Me.Label9)
+        Me.TabPage2.Controls.Add(Me.TextBox8)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(601, 359)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Restore"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button4.Location = New System.Drawing.Point(558, 67)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(26, 23)
+        Me.Button4.TabIndex = 35
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(116, 119)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(468, 225)
+        Me.ListBox1.TabIndex = 37
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(16, 119)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(77, 13)
+        Me.Label5.TabIndex = 36
+        Me.Label5.Text = "Backup Status"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(16, 93)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(91, 13)
+        Me.Label6.TabIndex = 34
+        Me.Label6.Text = "Connection String"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(116, 93)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(468, 20)
+        Me.TextBox5.TabIndex = 33
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(16, 67)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(69, 13)
+        Me.Label7.TabIndex = 32
+        Me.Label7.Text = "Backup Path"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(116, 67)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(436, 20)
+        Me.TextBox6.TabIndex = 31
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(16, 41)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(53, 13)
+        Me.Label8.TabIndex = 30
+        Me.Label8.Text = "Database"
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(116, 41)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(468, 20)
+        Me.TextBox7.TabIndex = 29
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(16, 15)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(38, 13)
+        Me.Label9.TabIndex = 28
+        Me.Label9.Text = "Server"
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(116, 15)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(468, 20)
+        Me.TextBox8.TabIndex = 27
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Controls.Add(Me.txtConnectionConnect)
+        Me.TabPage3.Controls.Add(Me.cmdConnect)
+        Me.TabPage3.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage3.Size = New System.Drawing.Size(601, 359)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "Connection"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'cmdConnect
+        '
+        Me.cmdConnect.Location = New System.Drawing.Point(19, 23)
+        Me.cmdConnect.Name = "cmdConnect"
+        Me.cmdConnect.Size = New System.Drawing.Size(75, 23)
+        Me.cmdConnect.TabIndex = 0
+        Me.cmdConnect.Text = "Connect"
+        Me.cmdConnect.UseVisualStyleBackColor = True
+        '
+        'txtConnectionConnect
+        '
+        Me.txtConnectionConnect.Location = New System.Drawing.Point(116, 25)
+        Me.txtConnectionConnect.Name = "txtConnectionConnect"
+        Me.txtConnectionConnect.Size = New System.Drawing.Size(468, 20)
+        Me.txtConnectionConnect.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(625, 433)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Database Backup and Restore"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        Me.TabPage3.ResumeLayout(False)
+        Me.TabPage3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtBackupServer As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtBackupConnectionString As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtBackupFileName As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtBackupDatabase As TextBox
+    Friend WithEvents lstBackupScript As ListBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents Label11 As Label
+    Friend WithEvents txtCatalogCatalog As TextBox
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents cmdConnect As Button
+    Friend WithEvents txtConnectionConnect As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,113 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Dim cnADONetConnection As New System.Data.SqlClient.SqlConnection()
+
+    Private m_cn As New SqlConnection()
+
+    m_cn.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="C\Users\Admin\Google Drive\Property\000 - Administration\Contacts.mdf";Integrated Security=True;Connect Timeout=30"
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles txtBackupConnectionString.TextChanged
+
+    End Sub
+
+    Private Sub Label9_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TextBox9_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Public Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim strServer, strDatabase, strBackupPath, strConnectionString, strBackupStatus As String
+
+        strServer = ""
+        strDatabase = ""
+        strBackupPath = ""
+        strConnectionString = ""
+        strBackupStatus = ""
+
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Dim fdFileDialog As OpenFileDialog = New OpenFileDialog()
+        Dim strBackupPath As String
+
+        fdFileDialog.Title = "Open File Dialog"
+        'fdFileDialog.InitialDirectory = "C:\"
+        fdFileDialog.Filter = "All files (*.*)|*.*|All files (*.*)|*.*"
+        fdFileDialog.FilterIndex = 2
+        fdFileDialog.RestoreDirectory = True
+
+        strBackupPath = fdFileDialog.FileName
+
+
+        If fdFileDialog.ShowDialog() = DialogResult.OK Then
+            strBackupPath = fdFileDialog.FileName
+            txtBackupFileName.Text = strBackupPath
+
+        End If
+
+        'MessageBox.Show(strBackupPath)
+
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Label10_Click(sender As Object, e As EventArgs) Handles Label10.Click
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub txtBackupServer_TextChanged(sender As Object, e As EventArgs) Handles txtBackupServer.TextChanged
+
+    End Sub
+
+    Private Sub cmdConnect_Click(sender As Object, e As EventArgs) Handles cmdConnect.Click
+
+        m_cn.Open()
+        ' txtConnectionConnect.Text = m_cn.RetrieveStatistics
+        MessageBox.Show("Button Clicked")
+    End Sub
+End Class
+
+'Private Sub EstablishDatabaseConnection()
+'Provider = SQLXMLOLEDB.3.0;Data Provider=SQLOLEDB;Data Source=myServerAddress;
+'Initial Catalog = myDataBase;User Id=myUsername;Password=myPassword;
+
+'strConnectionString = "'Provider=SQLXMLOLEDB.3.0; Data Provider=SQLOLEDB; Data Source=" & txtBackupServer.text & ";" &
+'       "Initial Catalog=" & txtBackupCatalog.text & ";Integrated Security=True/"
+'End Sub
+
